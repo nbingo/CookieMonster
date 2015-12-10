a3915d8e05cad1f49b11be258ce440d9e6dc3d6f
nbingo <nringach1@hwemail.com>
2015-12-10 14:15:38 -0800

    SortedSLLPQ is working!

--- Cookie Monster/src/SortedSLLPQ.java
+++ Cookie Monster/src/SortedSLLPQ.java
@@ -11,29 +11,60 @@ public class SortedSLLPQ<E extends Comparable<E>> implements MyPriorityQueue<E>
 		objectCount = 0;
 	}
 
+	@SuppressWarnings("unchecked")
 	public void add(E obj)
 	{
-
+		boolean added = false;
+		if (isEmpty())
+			queueHead = new ListNode(obj, null);
+		else if (objectCount == 1 || obj.compareTo((E)queueHead.getValue())<0)
+			if (obj.compareTo((E)queueHead.getValue())<0)
+				queueHead = new ListNode(obj, queueHead);
+			else
+				queueHead.setNext(new ListNode(obj, null));
+		else
+			for (ListNode node = queueHead; node != null && !added; node = node.getNext())
+				if (node.getNext() == null)
+				{
+					node.setNext(new ListNode(obj, null));
+					added = true;
+				}
+				else if (obj.compareTo((E)node.getNext().getValue())<0)
+				{
+					node.setNext(new ListNode(obj, node.getNext()));
+					added = true;
+				}
+				
+		objectCount++;
 	}
 
-	public E removeMin()
+	@SuppressWarnings("unchecked")
+	public E removeMin() throws IllegalStateException
 	{
-
+		if (isEmpty())
+			throw new IllegalStateException();
+		E obj = (E)queueHead.getValue();
+		queueHead = queueHead.getNext();
+		objectCount--;
+		return obj;
 	}
 
-	public E peek()
+	@SuppressWarnings("unchecked") 
+	public E peek() throws IllegalStateException
 	{
-
+		if (isEmpty())
+			throw new IllegalStateException();
+		return (E)queueHead.getValue();
 	}
 
 	public boolean isEmpty()
 	{
-
+		return objectCount == 0;
 	}
 
 	public int size()
 	{
-
+		return objectCount;
 	}
 
 	public String toString()
