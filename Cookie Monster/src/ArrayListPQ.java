d3080170566d2790ffc76c173d51ed024ae30251
nbingo <nringach1@hwemail.com>
2015-12-09 17:45:44 -0800

    Stuff might be working.

--- Cookie Monster/src/ArrayListPQ.java
+++ Cookie Monster/src/ArrayListPQ.java
@@ -11,27 +11,32 @@ public class ArrayListPQ<E extends Comparable<E>> implements MyPriorityQueue<E> 
 	
 	public void add(E obj)
 	{
-		
+		queue.add(obj);
 	}
 	
 	public E removeMin()
 	{
-		
+		E least = queue.get(0);
+		int i;
+		for (i = 0; i < queue.size()-1; i++)
+			if (least.compareTo(queue.get(i))<0)
+				least = queue.get(i);
+		return queue.remove(i);
 	}
 	
 	public E peek()
 	{
-		
+		return queue.get(0);
 	}
 	
 	public boolean isEmpty()
 	{
-		
+		return queue.size() == 0;
 	}
 	
 	public int size()
 	{
-		
+		return queue.size();
 	}
 	
 	public String toString()
--- Cookie Monster/src/UnsortedDLLPQ.java
+++ Cookie Monster/src/UnsortedDLLPQ.java
@@ -11,27 +11,48 @@ public class UnsortedDLLPQ<E extends Comparable<E>> implements MyPriorityQueue<E
 	
 	public void add(E obj)
 	{
-		
+		if (objectCount == 0)
+		{
+			queueHead = new ListNode2(obj);
+			queueHead.setNext(queueHead);
+			queueHead.setPrevious(queueHead);
+		}
+		else
+			queueHead = new ListNode2(obj, queueHead.getPrevious(), queueHead);
+		objectCount++;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public E removeMin()
 	{
-		
+		ListNode2 least = queueHead;
+		for (ListNode2 node = queueHead.getNext(); node != queueHead; node = node.getNext())
+			if (((E)least.getValue()).compareTo((E)node.getValue())>0)
+				least = node;
+		least.getPrevious().setNext(least.getNext());
+		least.getNext().setPrevious(least.getPrevious());
+		objectCount--;
+		return (E)least.getValue();
 	}
 	
+	@SuppressWarnings("unchecked")
 	public E peek()
 	{
-		
+		ListNode2 least = queueHead;
+		for (ListNode2 node = queueHead.getNext(); node != queueHead; node = node.getNext())
+			if (((E)least.getValue()).compareTo((E)node.getValue())>0)
+				least = node;
+		return (E)least.getValue();
 	}
 	
 	public boolean isEmpty()
 	{
-		
+		return objectCount == 0;
 	}
 	
 	public int size()
 	{
-		
+		return objectCount;
 	}
 	
 	public String toString()
